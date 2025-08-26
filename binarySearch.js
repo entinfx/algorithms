686c9d865cda7339d7068aac434a080e4de19ce7
entinfx <monkacry@gmail.com>
2025-08-27 03:38:19 +0500

    Simplified.

--- binarySearch.js
+++ binarySearch.js
@@ -3,41 +3,29 @@
 const sortedArray = [2, 4, 12, 63, 80, 128, 4091, 75390]
 const target = 128
 
-function findIndex(target, sortedArray) {
-    console.log('Starting searching for value ' + target + '.')
 
-    function binarySearch(startIndex, endIndex) {
-        if (startIndex > endIndex) {
-            console.log('Error: \'startIndex\' is greater than \'endIndex\'.')
-            return undefined
-        }
-
-        const currentMiddleIndex = Math.floor((startIndex + endIndex) / 2)
-        console.log('Current middle index: ' + currentMiddleIndex + '.')
-
-        const currentMiddleValue = sortedArray[currentMiddleIndex]
-        console.log('Current middle value: ' + currentMiddleValue + '.')
-
-        if (target === currentMiddleValue) {
-            console.log('Target ' + currentMiddleValue + ' found at index ' + currentMiddleIndex + '.')
-            return currentMiddleIndex
-        }
-
-        if (target < currentMiddleValue) {
-            console.log('Target ' + target + ' is less than current middle value ' + currentMiddleValue + '.')
-            console.log('Next, looking within index range ' + startIndex + '-' + (currentMiddleIndex - 1) + '.')
-            return binarySearch(startIndex, currentMiddleIndex - 1)
-        }
-
-        if (target > currentMiddleValue ) {
-            console.log('Target ' + target + ' is greater than current middle value ' + currentMiddleValue + '.')
-            console.log('Next, looking within index range ' + (currentMiddleIndex + 1) + '-' + endIndex + '.')
-            return binarySearch(currentMiddleIndex + 1, endIndex)
-        }
+function binarySearch(startIndex, endIndex) {
+    if (startIndex > endIndex) {
+        console.log('Error: \'startIndex\' is greater than \'endIndex\'.')
+        return undefined
     }
 
-    const index = binarySearch(0, sortedArray.length - 1)
-    console.log('Finished searching for value ' + target + '.' + ' Found at index ' + index + '.')
+    const currentMiddleIndex = Math.floor((startIndex + endIndex) / 2)
+    const currentMiddleValue = sortedArray[currentMiddleIndex]
+
+    if (target === currentMiddleValue) {
+        return currentMiddleIndex
+    }
+
+    if (target < currentMiddleValue) {
+        return binarySearch(startIndex, currentMiddleIndex - 1)
+    }
+
+    if (target > currentMiddleValue ) {
+        return binarySearch(currentMiddleIndex + 1, endIndex)
+    }
 }
 
-findIndex(target, sortedArray)
+const index = binarySearch(0, sortedArray.length - 1)
+console.log('Finished searching for value ' + target + '.' + ' Found at index ' + index + '.')
+
